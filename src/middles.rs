5855f3d5ff85be0d8f7519ecfc467d594029fe2d
limoiie <limo.iie4@gmail.com>
2022-12-15 18:51:57 +0800

    test: Add cases for middles

--- src/middles.rs
+++ src/middles.rs
@@ -668,3 +668,312 @@ pub(crate) mod server {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(test)]
+    mod test_client {
+        use std::io::Write;
+
+        use chain_ext::mongodb_gridfs::DatabaseExt;
+        use fake::Fake;
+        use tempfile::{tempdir, NamedTempFile};
+        use test_utilities::docker;
+
+        use super::*;
+
+        //noinspection DuplicatedCode
+        #[tokio::test]
+        async fn test_invoke_middle() {
+            let container = docker::Builder::new("mongo")
+                .name("cmdproxy-test-client")
+                .port_mapping(0, Some(27017))
+                .build_disposable()
+                .await;
+
+            let bucket = mongodb::Client::with_uri_str(container.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("cmdproxy-test-client-db")
+                .bucket(None);
+
+            let fake_workspace = tempdir().unwrap();
+
+            let mut fake_input = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+            let fake_output = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+
+            let fake_stdout = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+            let fake_stderr = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+
+            let fake_input_content = (30..50).fake::<String>();
+            let fake_stdout_content = (30..50).fake::<String>();
+
+            fake_input.write_all(fake_input_content.as_bytes()).unwrap();
+
+            let mut in_params = vec![];
+            let mut out_params = vec![];
+
+            let mut ipath = |path: &str, content: String| {
+                let param = Param::ipath(path);
+                in_params.push((param.clone(), content));
+                param
+            };
+
+            let mut opath = |path: &str, content: String| {
+                let param = Param::opath(path);
+                out_params.push((param.clone(), content));
+                param
+            };
+
+            let req = RunRequest {
+                command: Param::str("/bin/bash"),
+                args: vec![
+                    Param::str("-c"),
+                    Param::format(
+                        "echo '{content}' && cat {input} > {output}",
+                        HashMap::from([
+                            ("content", Param::str(fake_stdout_content.clone())),
+                            (
+                                "input",
+                                ipath(
+                                    fake_input.path().to_str().unwrap(),
+                                    fake_input_content.clone(),
+                                ),
+                            ),
+                            (
+                                "output",
+                                opath(
+                                    fake_output.path().to_str().unwrap(),
+                                    fake_input_content.clone(),
+                                ),
+                            ),
+                        ]),
+                    ),
+                ],
+                stdout: Some(opath(
+                    fake_stdout.path().to_str().unwrap(),
+                    fake_stdout_content.clone(),
+                )),
+                stderr: Some(opath(fake_stderr.path().to_str().unwrap(), String::new())),
+                cwd: None,
+                env: None,
+                to_downloads: None,
+                to_uploads: None,
+            };
+
+            {
+                let invoke_middle = client::ProxyInvokeMiddle::new(bucket.clone());
+                let wrapped_req = invoke_middle.transform_request(req).await;
+
+                assert!(matches!(wrapped_req.command,
+                    Param::StrParam { value } if value == "/bin/bash"));
+                assert!(matches!(
+                    wrapped_req.args.first().unwrap(),
+                    Param::StrParam { value } if value == "-c"
+                ));
+
+                let (in_param, out_param) = match wrapped_req.args.last().unwrap() {
+                    Param::FormatParam { args, .. } => {
+                        // assert file params have been transformed into cloud ones
+                        (args["input"].clone(), args["output"].clone())
+                    }
+                    _ => panic!(),
+                };
+                let stdout_param = wrapped_req.stdout.unwrap();
+                let stderr_param = wrapped_req.stderr.unwrap();
+
+                let wrapped_in_params = vec![in_param];
+                let wrapped_out_params = vec![out_param, stdout_param, stderr_param];
+
+                // assert file params have been transformed as cloud param
+                for (wrapped_in_param, (_, content)) in
+                    wrapped_in_params.iter().zip(in_params.iter())
+                {
+                    assert!(wrapped_in_param.is_input() && wrapped_in_param.is_cloud());
+
+                    // assert input files have been uploaded
+                    let uploaded_content = wrapped_in_param
+                        .download_to_string(bucket.clone())
+                        .await
+                        .unwrap();
+                    assert_eq!(content, &uploaded_content);
+                }
+
+                // assert file params have been transformed as cloud param
+                for wrapped_out_param in wrapped_out_params {
+                    assert!(wrapped_out_param.is_output() && wrapped_out_param.is_cloud());
+                }
+
+                // mimic server to upload output files after running
+                for (out_param, content) in &out_params {
+                    out_param
+                        .upload_from_string(bucket.clone(), content.as_str())
+                        .await
+                        .unwrap();
+                }
+            }
+
+            // assert all the inputs have been removed from the cloud
+            for (in_param, _content) in in_params {
+                assert!(!in_param.exists_on_cloud(bucket.clone()).await.unwrap());
+            }
+
+            // assert all the outputs have been downloaded, and been removed from the cloud
+            for (out_param, content) in out_params {
+                assert!(!out_param.exists_on_cloud(bucket.clone()).await.unwrap());
+                assert_eq!(
+                    content,
+                    std::fs::read_to_string(out_param.filepath()).unwrap()
+                );
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test_server {
+        use std::io::Write;
+
+        use chain_ext::mongodb_gridfs::DatabaseExt;
+        use fake::Fake;
+        use tempfile::{tempdir, NamedTempFile};
+        use test_utilities::docker;
+
+        use super::*;
+
+        //noinspection DuplicatedCode
+        #[tokio::test]
+        async fn test_invoke_middle() {
+            let container = docker::Builder::new("mongo")
+                .name("cmdproxy-test-server")
+                .port_mapping(0, Some(27017))
+                .build_disposable()
+                .await;
+
+            let bucket = mongodb::Client::with_uri_str(container.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("cmdproxy-test-server-db")
+                .bucket(None);
+
+            let fake_workspace = tempdir().unwrap();
+
+            let mut fake_input = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+            let fake_output = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+
+            let fake_stdout = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+            let fake_stderr = NamedTempFile::new_in(fake_workspace.path()).unwrap();
+
+            let fake_input_content = (30..50).fake::<String>();
+            let fake_stdout_content = (30..50).fake::<String>();
+
+            fake_input.write_all(fake_input_content.as_bytes()).unwrap();
+
+            let mut in_params = vec![];
+            let mut out_params = vec![];
+
+            let mut ipath = |path: &str, content: String| {
+                let param = Param::ipath(path).as_cloud();
+                in_params.push((param.clone(), content));
+                param
+            };
+
+            let mut opath = |path: &str, content: String| {
+                let param = Param::opath(path).as_cloud();
+                out_params.push((param.clone(), content));
+                param
+            };
+
+            let req = RunRequest {
+                command: Param::str("/bin/bash"),
+                args: vec![
+                    Param::str("-c"),
+                    Param::format(
+                        "echo '{content}' && cat {input} > {output}",
+                        HashMap::from([
+                            ("content", Param::str(fake_stdout_content.clone())),
+                            (
+                                "input",
+                                ipath(
+                                    fake_input.path().to_str().unwrap(),
+                                    fake_input_content.clone(),
+                                ),
+                            ),
+                            (
+                                "output",
+                                opath(
+                                    fake_output.path().to_str().unwrap(),
+                                    fake_input_content.clone(),
+                                ),
+                            ),
+                        ]),
+                    ),
+                ],
+                stdout: Some(opath(
+                    fake_stdout.path().to_str().unwrap(),
+                    fake_stdout_content.clone(),
+                )),
+                stderr: Some(opath(fake_stderr.path().to_str().unwrap(), String::new())),
+                cwd: None,
+                env: None,
+                to_downloads: None,
+                to_uploads: None,
+            };
+
+            // mimic client upload input files
+            for (param, content) in &in_params {
+                param
+                    .upload_from_string(bucket.clone(), content)
+                    .await
+                    .unwrap();
+            }
+
+            {
+                let server_tempdir = tempdir().unwrap();
+                let invoke_middle = server::ProxyInvokeMiddle::new(bucket.clone(), server_tempdir);
+                let run_spec = invoke_middle.transform_request(req).await;
+
+                assert_eq!(run_spec.command, "/bin/bash");
+                assert_eq!(run_spec.args.first().unwrap(), "-c");
+
+                let pat = regex::Regex::new(
+                    "echo '(?P<content>.*)' && cat (?P<input>.*) > (?P<output>.*)",
+                )
+                .unwrap();
+                let cap = pat.captures(run_spec.args.last().unwrap()).unwrap();
+
+                assert_eq!(cap.len(), 4);
+                assert!(cap.name("content").is_some());
+                assert_eq!(
+                    cap.name("content").unwrap().as_str(),
+                    fake_stdout_content.as_str()
+                );
+                assert!(cap.name("input").is_some());
+                assert!(cap.name("output").is_some());
+
+                // mimic server to generate output files
+                std::fs::write(
+                    cap.name("output").unwrap().as_str(),
+                    fake_input_content.as_str(),
+                )
+                .unwrap();
+                std::fs::write(
+                    run_spec.stdout.unwrap().as_str(),
+                    fake_stdout_content.as_str(),
+                )
+                .unwrap();
+                std::fs::write(run_spec.stderr.unwrap().as_str(), "").unwrap();
+            }
+
+            // assert all the outputs have been uploaded
+            for (out_param, content) in out_params {
+                assert!(out_param.exists_on_cloud(bucket.clone()).await.unwrap());
+                assert_eq!(
+                    content,
+                    out_param.download_to_string(bucket.clone()).await.unwrap()
+                );
+            }
+        }
+    }
+}
